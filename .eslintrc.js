38a74eea1be2a8f3f2844d71dfd519015fa7c20b
Canturilsnaik <italo.cantur@gmail.com>
2021-02-02 10:57:57 -0300

    :wrench: chore: Update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended',
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
+    'no-use-before-define': 'off',
   },
 };
